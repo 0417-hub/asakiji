22869ade6863893937e8ff2f5af7c26e11c1a9b7
KazuyaMutoh <mfshm0417@gmail.com>
2023-09-04 22:38:55 +0900

    Add: ループされているeventがLine::Bot::Event::Messageクラスであるかのチェック

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -2,6 +2,11 @@ class LineBotController < ApplicationController
   def callback
     body = request.body.read
     events = client.parse_events_from(body)
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
   end
 
   private
